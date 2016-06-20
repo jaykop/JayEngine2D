bc4a92a7684958d53c85936d782beccabfb095b7
jaykop <jaykop.jy@gmail.com>
2016-06-21 02:08:47 +0900

    Polishing text class
    Polishing text class

--- GameEngine/GameEngine/Source/Engine/Graphic/Text.cpp
+++ GameEngine/GameEngine/Source/Engine/Graphic/Text.cpp
@@ -3,7 +3,7 @@
 \file   Text.cpp
 \author Jeong Juyong
 \par    email: jeykop14\@gmail.com
-\date   2016/06/19(yy/mm/dd)
+\date   2016/06/20(yy/mm/dd)
 
 \description
 Contains Text's class and member functions
@@ -14,51 +14,128 @@ All content (C) 2016 DigiPen (USA) Corporation, all rights reserved.
 
 #include "Text.h"
 
-void Text::Print(const font_data &ft_font, float x, float y, const char* fmt)
+Text::Text(void)
+:m_position(vec3()), m_scale(vec3(1, 1, 0)), 
+m_rotation(0), m_color(vec4(1, 1, 1, 1))
+{}
+
+Text::~Text(void)
+{}
+
+Text::font_data::font_data(void)
 {
-	UNREFERENCED_PARAMETER(ft_font);
-	UNREFERENCED_PARAMETER(x);
-	UNREFERENCED_PARAMETER(y);
-	UNREFERENCED_PARAMETER(fmt);
+	SetFont();
 }
 
-void Text::Print(TCHAR* string)
+int Text::font_data::Next_p2(int a)
 {
-	for (int index = 0; index < lstrlen(string); ++index)
-	{
-		FT_Load_Char(m_face, string[index], FT_LOAD_RENDER | FT_LOAD_NO_BITMAP);
-
-		m_width = m_face->glyph->bitmap.width;
-		m_height = m_face->glyph->bitmap.rows;
-
-		for (int y = 0; y < m_height; ++y)
-		{
-			for (int x = 0; x < m_width; ++x)
-			{
-				m_color = vec4(255 - m_face->glyph->bitmap.buffer[y * m_width + x],
-					255 - m_face->glyph->bitmap.buffer[y * m_width + x],
-					255 - m_face->glyph->bitmap.buffer[y * m_width + x],
-					255 - m_face->glyph->bitmap.buffer[y * m_width + x]);
-
-				if (m_color.x != 255 ||
-					m_color.y != 255 ||
-					m_color.z != 255 ||
-					m_color.w != 255);
-			}
-		}
-	}
+	int rval = 1;
+	while (rval < a) rval << 1;
+	return rval;
 }
 
-void Text::SetFont(FT_Library lib, const char* fontDir)
+void Text::font_data::SetFont(const char* fontDir, unsigned size)
 {
-	FT_New_Face(lib, fontDir, 0, &m_face);
+	texture = new GLuint[256];
+	this->height = float(size);
+	
+	FT_Library lib;
+	if (FT_Init_FreeType(&lib))
+		throw std::runtime_error("FT_Init_FreeType failed!");
+
+	FT_Face face = 0;
+	if (FT_New_Face(lib, fontDir, 0, &face))
+		throw std::runtime_error("FT_New_Face failed (there is probably a problem with font file)");
+
+	FT_Set_Char_Size(face, size << 6, size << 6, 96, 96);
+	list_base = glGenLists(256);
+
+	glGenTextures(256, (GLuint*)texture);
+	for (unsigned char i = 0; i < 256; ++i)
+		Make_dlist(face, i, list_base, (GLuint*)&texture);
+
+	FT_Done_Face(face);
+	FT_Done_FreeType(lib);
 }
 
-void Text::SetFontSize(HDC hdc, int width, int height)
+Text::font_data::~font_data(void)
 {
-	FT_Set_Char_Size(m_face, 128 * 64, 0, 
-		GetDeviceCaps(hdc, LOGPIXELSX), GetDeviceCaps(hdc, LOGPIXELSY));
-
-	m_width = width;
-	m_height = height;
+	Clear();
 }
+
+void Text::font_data::Clear(void)
+{
+	glDeleteLists(list_base, 256);
+	glDeleteTextures(256, (GLuint*)&texture);
+	delete[] texture;
+}
+
+void Text::font_data::Make_dlist(FT_Face face, char ch, GLuint list_base, GLuint* tex_base)
+{
+	if (FT_Load_Glyph(face, FT_Get_Char_Index(face, ch), FT_LOAD_DEFAULT))
+		throw std::runtime_error("FT_Load_Glyph failed");
+
+	FT_Glyph glyph;
+	if (FT_Get_Glyph(face->glyph, &glyph))
+		throw std::runtime_error("FT_Load_Glyph failed");
+
+	FT_Glyph_To_Bitmap(&glyph, ft_render_mode_normal, 0, 1);
+	FT_BitmapGlyph bitmap_glyph = FT_BitmapGlyph(glyph);
+	FT_Bitmap& bitmap = bitmap_glyph->bitmap;
+
+	int width = Next_p2(bitmap.width);
+	int height = Next_p2(bitmap.rows);
+
+	GLubyte* expanded_data = new GLubyte[2 * width * height];
+
+}
+
+//void Text::Print(const font_data &ft_font, float x, float y, const char* fmt)
+//{
+//	UNREFERENCED_PARAMETER(ft_font);
+//	UNREFERENCED_PARAMETER(x);
+//	UNREFERENCED_PARAMETER(y);
+//	UNREFERENCED_PARAMETER(fmt);
+//}
+
+//void Text::Print(TCHAR* string)
+//{
+//	for (int index = 0; index < lstrlen(string); ++index)
+//	{
+//		FT_Load_Char(m_face, string[index], FT_LOAD_RENDER | FT_LOAD_NO_BITMAP);
+//
+//		m_width = m_face->glyph->bitmap.width;
+//		m_height = m_face->glyph->bitmap.rows;
+//
+//		for (int y = 0; y < m_height; ++y)
+//		{
+//			for (int x = 0; x < m_width; ++x)
+//			{
+//				m_color = vec4(255 - m_face->glyph->bitmap.buffer[y * m_width + x],
+//					255 - m_face->glyph->bitmap.buffer[y * m_width + x],
+//					255 - m_face->glyph->bitmap.buffer[y * m_width + x],
+//					255 - m_face->glyph->bitmap.buffer[y * m_width + x]);
+//
+//				if (m_color.x != 255 ||
+//					m_color.y != 255 ||
+//					m_color.z != 255 ||
+//					m_color.w != 255)
+//					SetPixelV(hdc, penx + x, y, RGB(m_color.x, m_color.y, m_color.z, m_color.w));
+//			}
+//		}
+//	}
+//}
+//
+//void Text::SetFont(FT_Library lib, const char* fontDir)
+//{
+//	
+//}
+//
+//void Text::SetFontSize(HDC hdc, int width, int height)
+//{
+//	FT_Set_Char_Size(m_face, 128 * 64, 0, 
+//		GetDeviceCaps(hdc, LOGPIXELSX), GetDeviceCaps(hdc, LOGPIXELSY));
+//
+//	m_width = width;
+//	m_height = height;
+//}
--- GameEngine/GameEngine/Source/Engine/Graphic/Text.h
+++ GameEngine/GameEngine/Source/Engine/Graphic/Text.h
@@ -3,7 +3,7 @@
 \file   Text.h
 \author Jeong Juyong
 \par    email: jeykop14\@gmail.com
-\date   2016/06/19(yy/mm/dd)
+\date   2016/06/20(yy/mm/dd)
 
 \description
 Contains Text's class and members
@@ -38,30 +38,45 @@ All content (C) 2016 DigiPen (USA) Corporation, all rights reserved.
 
 using namespace std;
 
-struct font_data {
-	float height;
-	GLuint *texture;
-	GLuint list_base;
-
-	void init(const char* font_name, unsigned height);
-	void clean(void);
-};
-
 class Text
 {
 public:
 
-	void Print(TCHAR* string);
-	void Print(const font_data &ft_font, float x, float y, const char* fmt);
-	void SetFont(FT_Library lib, const char* fontDir
-		 = "Resource/Font/Default.ttf");
-	void SetFontSize(HDC hdc, int width, int height);
+	Text();
+	~Text(void);
+
+
+	struct font_data {
+		float  height;
+		GLuint *texture;	//Texture *texture;
+		GLuint list_base;
+
+		font_data(void);
+		~font_data(void);
+
+		void Make_dlist(FT_Face face, char ch, GLuint list_base, GLuint* tex_base);
+		void SetFont(const char* fontDir
+			= "Resource/Font/Default.ttf", unsigned size = 36);
+
+		int Next_p2(int a);
+		void Clear(void);
+	};
+
+	//void Print(TCHAR* string);
+	//void Print(const font_data &ft_font, float x, float y, const char* fmt);
+
+	//void SetFontSize(HDC hdc, int width, int height);
 
 private:
 
-	FT_Face m_face;
-	int m_width, m_height;
+	vec3 m_scale;
 	vec4 m_color;
+	vec3 m_position;
+	float m_rotation;
+	float m_fontSize;
+
+	FT_Face m_face;
+	font_data m_font;
 };
 
 #endif // _FREETYPE_TEXT_H_
